fb2da2531402cf7e12f9af01f27a14a4279a52c5
Reza Aghajani <rezaaghajani@protonmail.com>
2021-12-28 16:10:26 +0330

    Complete the x Calculating Algorithm

--- scripts/calculation-script.js
+++ scripts/calculation-script.js
@@ -1,4 +1,7 @@
 let lessons = [[3,"riazi"],[2,"masaleh"],[1,"Danesh Khanevade"],[3,"fizik"]];
+let hours = [5,4,7,8,2,1,10] 
+let lessonsHours = [];
+let plannerHours = []
 
 let sorting = (array) => {
     array.sort(function(a, b) {
@@ -11,5 +14,40 @@ let sorting = (array) => {
 
 sorting(lessons);
 
-console.log(lessons);
+let timePerDay = () => {
+    let sum = [];
+    for (let i = 0; i < hours.length; i++) {
+        sum.push(hours[i] * 60 / 9);
+    }
+    let perDay = sum;
+    return perDay;
+}
+
+// console.log(timePerDay(hours))
+
+
+let xCalculate = () => {
+    var x  = []
+    var y = []
+    //x = Math.round(x);
+    // var x  = timePerDay(hours) / sum;    
+    
+    for (let i = 0; i < hours.length; i++) {
+        for(let z = 0; z < lessons.length; z++) {
+            y.push([Math.round(lessons[z][0] *  timePerDay()[z]), lessons[z][1] ])
+        }
+
+        for(let t = 0; t < lessons.length; t++) {
+            x.push(i,y[t])
+        }
+
+        
+    }
+
+    return x;
+
+
+}
+
+console.log(xCalculate())
 
